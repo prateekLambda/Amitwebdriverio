a9485a5b1071cdd8ae10246304182f45b335ba55
prateekLambda <47247309+prateekLambda@users.noreply.github.com>
2020-10-20 19:21:57 +0530

    Update single_test.js

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -4,9 +4,9 @@ describe('Google Search Function', () => {
   it('can find search results', () => {
    // browser.url('https://www.aajtak.in/');
     //waitforTimeout: 10000,
-    browser.url('https://angular-file-upload.appspot.com/');
-    const input = $("input[name='userName']");
-    input.setValue('test123');
+    browser.url('https://www.rbauction.com/');
+    const input = $("#signInBtn");
+    input.click();
 
     const title = browser.getTitle();
     //assert.equal(title, 'Google');
