c014775d0aa3238785ecb59cda9ab19633e3702f
prateekLambda <47247309+prateekLambda@users.noreply.github.com>
2021-09-20 18:26:31 +0530

    Update single_test.js

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -2,15 +2,8 @@ const assert = require('assert');
 
 describe('Google Search Function', () => {
   it('can find search results', () => {
-   // browser.url('https://www.aajtak.in/');
-    //waitforTimeout: 10000,
-    browser.url('https://angular-file-upload.appspot.com/');
-    const input = $("input[name='userName']");
-    input.setValue('test123');
-
+    browser.url('https://www.google.com/ncr');
     const title = browser.getTitle();
-    //assert.equal(title, 'Google');
-    //browser.url('https://www.aajtak.in/');
-    // browser.url('http://localhost.lambdatest.com/');
+    assert.equal(title, 'Google');
   });
 });
